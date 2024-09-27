579bce7b0d0ff4b530e055daecb44c2efc370dc9
maaz <maazkamran65@gmail.com>
2024-09-27 13:00:18 +0500

    ffinal

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 // Generate JWT Token
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+    return jwt.sign({ id }, 'Secret', { expiresIn: '30d' });
 };
 
 // Register user
