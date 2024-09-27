5eb902bac64be8d4908dd5b07de483faf5848a0b
maaz <maazkamran65@gmail.com>
2024-09-27 12:41:04 +0500

    final

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
+
 const connectDB = async () => {
-    const MONGO_URI=process.env.MONGO_URI;
+    const MONGO_URI = process.env.MONGO_URI;
+
     try {
-        const conn = await mongoose.connect("mongodb+srv://maaz:mik%40ilo52@movie.zh3bs.mongodb.net/pizzaApp", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        const conn = await mongoose.connect(MONGO_URI);
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
     }
 };
+
 module.exports = connectDB;
+
